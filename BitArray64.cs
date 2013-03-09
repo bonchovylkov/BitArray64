18ca541a290545d9d6627a3b8bda8f0e6348c293
boncho valkov <bonchovylkov@abv.bg>
2013-03-09 14:42:00 +0200

    Homework Exersise 5

---
+++ BitArray64.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BitArray64 : IEnumerable<int> 
+    {
+       public ulong Number { get; set; }
+       public BitArray64(ulong number)
+       {
+           this.Number = number;
+       }
+       public IEnumerator<int> GetEnumerator()
+       {
+
+           for (int i = 0; i < 64; i++)
+           {
+               ulong checkBitOnPosition = Number & ((ulong)1 << i);
+               if (checkBitOnPosition>0)
+               {
+                   yield return 1;
+               }
+               else
+               {
+                   yield return 0;
+               }
+           }
+       }
+       IEnumerator IEnumerable.GetEnumerator()
+       {
+           return this.GetEnumerator();
+       }
+       public override bool Equals(object number)
+       {
+           BitArray64 justNumber = number as BitArray64;
+           if (justNumber==null)
+           {
+               return false;
+           }
+           if (this.Number!=justNumber.Number)
+           {
+               return false;
+           }
+           return true;
+       }
+       public override int GetHashCode()
+       {
+           return ((int)Number^(int)(Number*Number*Number));
+       }
+       public static bool operator ==(BitArray64 numberOne, BitArray64 numberTwo)
+       {
+           return BitArray64.Equals(numberOne, numberTwo);
+       }
+       public static bool operator !=(BitArray64 numberOne, BitArray64 numberTwo)
+       {
+           return !(BitArray64.Equals(numberOne, numberTwo));
+       }
+
+    }
+
+
+
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3.bitArrayClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            BitArray64 someNumber = new BitArray64(15);
+            BitArray64 otherNumber = new BitArray64(150);
+            //chech IEnumerable
+            StringBuilder str = new StringBuilder();
+            foreach (var bit in someNumber)
+            {
+                
+                str.Append(bit);
+            }
+            string bitReperentation = str.ToString();
+
+            char[] arrayBits = bitReperentation.ToCharArray();
+            Array.Reverse(arrayBits);
+            string finalBitRep = new string(arrayBits);
+            finalBitRep = finalBitRep.TrimStart('0');
+            Console.WriteLine(finalBitRep);
+            //check equals and hashcode
+            Console.WriteLine(someNumber.Equals(otherNumber));
+            Console.WriteLine(someNumber.GetHashCode());
+            Console.WriteLine(otherNumber.GetHashCode());
+
+            //chech == and !=
+            Console.WriteLine(someNumber==otherNumber);
+            Console.WriteLine(someNumber!=otherNumber);
+        }
+    }
+}
